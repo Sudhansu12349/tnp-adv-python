db45da9213577f50156df997cc313e9c65bad0d7
Sudhansu12349 <sudhansuraj343@gmail.com>
2026-03-13 12:48:36 +0530

    Add longestCommonPrefix function
    Implement function to find longest common prefix among strings.

---
+++ hackwith_infy/LC_14_LCP.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+        string ans;
+        for(int i =0;i<strs[0].size();i++){
+            for(int j=1;j<strs.size();j++){
+               if(i>=strs[j].size()||strs[0][i]!=strs[j][i]){
+                  return ans;
+
+                }
+            }
+            ans+=strs[0][i];
+        }
+         return ans;
+
+}
+int main(){
+
+}
