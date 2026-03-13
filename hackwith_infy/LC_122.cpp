b20634e92bbacaf60527f35aff44d55d2f4205ad
Sudhansu12349 <sudhansuraj343@gmail.com>
2026-03-13 12:59:39 +0530

    Best time to buy and sell stocks 2
    Implement maxProfit function to calculate maximum profit from stock prices.

---
+++ hackwith_infy/LC_122.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ int maxProfit(vector<int>& prices) {
+        
+        vector<int> profit;
+        
+        for( size_t i = 0 ; i < prices.size()-1 ; i++ ){
+            
+            if( prices[i] < prices[i+1] ){
+                profit.push_back( prices[i+1] - prices[i] );
+            }
+            
+        }
+        return accumulate( profit.begin(), profit.end(), 0);
+}
+int main(){
+
+}
